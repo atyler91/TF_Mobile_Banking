f6b233318c729f72c9b5f0f05f2337de252b8610
atyler91 <atyler91@gmail.com>
2018-04-20 13:39:52 -0400

    tabMode - Andrew

--- TF_Mobile_Banking/TF_Mobile_Banking/MainPage.xaml.cs
+++ TF_Mobile_Banking/TF_Mobile_Banking/MainPage.xaml.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace TF_Mobile_Banking
 {
-	public partial class MainPage : TabbedPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
 	{
 		public MainPage()
 		{
